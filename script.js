7a16001f2e060b1f004344e4ecd8c35ee3560d29
talaybekovErlan <erlantalaybekov083@gmail.com>
2022-12-27 23:34:13 +0600

    danya durak

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log ("sda");
+
+// let el = document.querySelector('.header--gradient');
+// document.getElementsByClassName('.header--gradient').style.background-colr = "blue";
+// console.log ("sda");
+
+// onclick =   document.getElementById('id1') .style.background = 'url(/anticafe/img/header-bg.jpg)';
+document.getElementById('id1') .style.background = 'url(/anticafe/img/header-bg.jpg)';
